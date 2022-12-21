05ab2de72ea4d96ccf2e03401ccf9700a2b966dc
Nurcan Şensoy <nurcansensoyy@gmail.com>
2022-12-21 14:32:14 +0300

    popup yapımı

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,36 @@
-<script setup>
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
-
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <div class="home">
+    <h1>Tariflerim</h1>
+    <button>Yeni Tarif Ekle</button>
+
+    <div class="tarifler">
+      <!-- Tarifler -->
+    </div>
+    <div class="tarif-popup">
+      <div class="popup-konu">
+        <h2>Yeni Tarif Ekle</h2>
+        <form @submit.prevent=""></form>
+        <div class="grup">
+          <label>Başlık</label>
+          <input type="text" />
+        </div>
+        <div class="grup">
+          <label>Açıklama</label>
+          <textarea name="" id="" cols="30" rows="10"></textarea>
+        </div>
+        <div class="grup">
+          <label>İçindekiler</label>
+          <input type="text" />
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
+
+<script>
+import { def } from "@vue/shared";
+
+export default {
+  name: "Home",
+};
+</script>
